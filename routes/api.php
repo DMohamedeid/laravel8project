a06ec995d3f8da1cfd1cc4275b9caa3e1f8092f1
Mohamed Eid <m.mohamedeid11@gmail.com>
2022-09-04 11:39:08 +0200

    Routing

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,40 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/home',function(){
+   return ' Welcome back Fiery Dragon ';
+});
+
+Route::get('/home1/{name}',function($name){
+    return ' rout with required parameter ' . $name;
+});
+
+Route::get('/home2/{name?}',function($name = null){
+    return ' rout with optional parameter ' . $name;
+});
+
+Route::get('/home3/{name}',function($name = null){
+    return ' rout with optional parameter ' . $name;
+})->where('name' , '[a-zA-Z]+');  //they will no longer working because of the app service provider
+
+Route::get('/home4/{id}',function($id = null){
+    return ' rout with optional parameter ' . $id;
+})->where('id' , '[0-9]+');     //they will no longer working because of the app service provider
+
+//hint don't ever use the service provider unless you want to accept the same functionality to all routes
+
+Route::get('/home5/{name}',function($name = null){
+    return ' Route with route constraint pattern from RouteServiceProvider ' . $name;
+});
+
+Route::get('/home6/{id}',function($id = null){
+    return ' Route with route constraint pattern from RouteServiceProvider ' . $id;
+});
+
+Route::match(['get' , 'put' ,'post'] ,'/home7',function(Request $request){
+    return 'Here match return the http with the method we choose' . $request;
+});
+
+Route::any('home8', function (Request $request){
+   return 'this any accept all the requests post get put delete the method is --> ' .$request->method();
+});
